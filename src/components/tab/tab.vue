46e9f514f1fd90f415d1c68b8cf64b5385ad4cac
Administrator <1851924429@qq.com>
2018-08-19 09:02:05 +0800

    增加配置文件

---
+++ src/components/tab/tab.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="tab">
+    <router-link tag="div" class="tab-item" to="/recommend">
+
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tab'
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+import Rank from '@/components/rank/rank'
+import Search from '@/components/search/search'
+import Singer from '@/components/singer/singer'
+import Recommend from '@/components/recommend/recommend'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/rank',
+      component: Rank
+    },
+    {
+      path: '/search',
+      component: Search
+    },
+    {
+      path: '/singer',
+      component: Singer
+    },
+    {
+      path: '/recommend',
+      component: Recommend
     }
   ]
 })
